c23b379bca4347f7eeb65d06cb6c835df6355671
RafFuentes <107508494+RafFuentes@users.noreply.github.com>
2023-05-25 21:36:57 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,92 @@
+    var carr = 0
+    let carrito = []
+    let comprado = []
+
+    //Saber si el producto esta en el carrito
+
+    if(comprado.find(i => (i === putin))){
+        document.getElementById('B1').innerText="Comprado"
+        document.getElementById('B1').disabled == true;
+        document.getElementById('parrafito1').innerText='El presidente ruso, Vladimir Putin, firmó este viernes un decreto por el que se aumenta formalmente a cadena perpetua la pena máxima por traición, en el marco de una campaña para reprimir la disidencia desde el inicio de la guerra en Ucrania.'
+    }
+    
+    if(comprado.find(i => (i === mordo))){
+        document.getElementById('B2').innerText="Comprado"
+        document.getElementById('B2').disabled == true;
+        document.getElementById('parrafito1').innerText='La influenza aviar (IA) es una enfermedad viral altamente contagiosa que afecta tanto a las aves domésticas como a las silvestres. Aunque con menos frecuencia, también se han aislado virus de influenza aviar en especies de mamíferos, así como en seres humanos.'
+    }
+
+    if(comprado.find(i => (i === litio))){
+        document.getElementById('B3').innerText="Comprado"
+        document.getElementById('B3').disabled == true;
+        document.getElementById('parrafito3').innerText='El objetivo de esta Estrategia Nacional es aumentar las riquezas para el país, desarrollando una industria clave como paso fundamental para vincular el desarrollo económico de Chile con el cambio hacia una economía Comprarde a nivel global. La alta demanda mundial, los elevados precios y las grandes reservas del litio en nuestro país nos permiten ser optimistas y, a la vez, nos llaman a actuar con sentido de urgencia. Es necesario incorporar capital, tecnología, sostenibilidad y agregación de valor, en armonía con las comunidades. Con tal propósito, el desarrollo de la industria del litio será liderado por el Estado e involucrará al sector privado en todo el proceso productivo.'
+    }
+
+    if(comprado.find(i => (i === lluvia))){
+        document.getElementById('B4').innerText="Comprado"
+        document.getElementById('B4').disabled == true;
+        document.getElementById('parrafito4').innerText='Se pronostican intensas lluvias para los proximos dias en la region de valparaiso. La temperatura media de la ciudad será en promedio de 14° con fuertes vientos y chuvascos.'
+    }
+
+    if(comprado.find(i => (i === contam))){
+        document.getElementById('B5').innerText="Comprado"
+        document.getElementById('B5').disabled == true;
+        document.getElementById('parrafito5').innerText='La contaminación es uno de los problemas ambientales más importantes que afectan a nuestro planeta y surge cuando se produce un desequilibrio, como resultado de la adición de cualquier sustancia al medio ambiente, en cantidad tal, que cause efectos adComprarsos en el hombre, en los animales, vegetales o materiales ...'
+    }
+
+    if(comprado.find(i => (i === terremoto))){
+        document.getElementById('B6').innerText="Comprado"
+        document.getElementById('B6').disabled == true;
+        document.getElementById('parrafito6').innerText='Un sismo de magnitud 5,8 sacudió este martes la zona de Balao, en la provincia costera de Guayas, en el suroeste de Ecuador, sin que hasta el momento se haya informado de víctimas ni daños materiales, pero que corresponde a un sector en el que se registró un terremoto de 6,5 el pasado 18 de marzo.'
+    }
+
+    if(comprado.find(i => (i === choque))){
+        document.getElementById('B7').innerText="Comprado"
+        document.getElementById('B7').disabled == true;
+        document.getElementById('parrafito7').innerText='Los hechos ocurrieron cerca de las 08:00 horas a la altura del kilómetro 4 de la ruta que une a Vilcún y Cajón, donde por causas que se investigan, un vehículo menor terminó impactando a un microbus que se trasladaba sin pasajeros.'
+    }
+
+    if(comprado.find(i => (i === farandula))){
+        document.getElementById('B8').innerText="Comprado"
+        document.getElementById('B8').disabled == true;
+        document.getElementById('parrafito8').innerText='Un fuerte descargo es el que realizó la bailarina Valentina Roth en Instagram, donde solicitó a los programas de farándula que no la nombraran más.'
+    }
+
+    //Saber si el producto esta comprado
+
+    if(comprado.find(i => (i === litio))){
+        document.getElementById('B3').innerText="Comprado"
+        document.getElementById('B3').disabled == true;
+        document.getElementById('parrafito3').innerText='El objetivo de esta Estrategia Nacional es aumentar las riquezas para el país, desarrollando una industria clave como paso fundamental para vincular el desarrollo económico de Chile con el cambio hacia una economía Comprarde a nivel global. La alta demanda mundial, los elevados precios y las grandes reservas del litio en nuestro país nos permiten ser optimistas y, a la vez, nos llaman a actuar con sentido de urgencia. Es necesario incorporar capital, tecnología, sostenibilidad y agregación de valor, en armonía con las comunidades. Con tal propósito, el desarrollo de la industria del litio será liderado por el Estado e involucrará al sector privado en todo el proceso productivo.'
+    }
+
+    if(comprado.find(i => (i === lluvia))){
+        document.getElementById('B4').innerText="Comprado"
+        document.getElementById('B4').disabled == true;
+        document.getElementById('parrafito4').innerText='Se pronostican intensas lluvias para los proximos dias en la region de valparaiso. La temperatura media de la ciudad será en promedio de 14° con fuertes vientos y chuvascos.'
+    }
+
+    if(comprado.find(i => (i === contam))){
+        document.getElementById('B5').innerText="Comprado"
+        document.getElementById('B5').disabled == true;
+        document.getElementById('parrafito5').innerText='La contaminación es uno de los problemas ambientales más importantes que afectan a nuestro planeta y surge cuando se produce un desequilibrio, como resultado de la adición de cualquier sustancia al medio ambiente, en cantidad tal, que cause efectos adComprarsos en el hombre, en los animales, vegetales o materiales ...'
+    }
+
+    if(comprado.find(i => (i === terremoto))){
+        document.getElementById('B6').innerText="Comprado"
+        document.getElementById('B6').disabled == true;
+        document.getElementById('parrafito6').innerText='Un sismo de magnitud 5,8 sacudió este martes la zona de Balao, en la provincia costera de Guayas, en el suroeste de Ecuador, sin que hasta el momento se haya informado de víctimas ni daños materiales, pero que corresponde a un sector en el que se registró un terremoto de 6,5 el pasado 18 de marzo.'
+    }
+
+    if(comprado.find(i => (i === choque))){
+        document.getElementById('B7').innerText="Comprado"
+        document.getElementById('B7').disabled == true;
+        document.getElementById('parrafito7').innerText='Los hechos ocurrieron cerca de las 08:00 horas a la altura del kilómetro 4 de la ruta que une a Vilcún y Cajón, donde por causas que se investigan, un vehículo menor terminó impactando a un microbus que se trasladaba sin pasajeros.'
+    }
+
+    if(comprado.find(i => (i === farandula))){
+        document.getElementById('B8').innerText="Comprado"
+        document.getElementById('B8').disabled == true;
+        document.getElementById('parrafito8').innerText='Un fuerte descargo es el que realizó la bailarina Valentina Roth en Instagram, donde solicitó a los programas de farándula que no la nombraran más.'
+ 
+    }
